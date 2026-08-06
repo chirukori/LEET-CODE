bb312687dc736535b4c3d9a8e9fbb69dc114fe6f
CHIRANTHANA K KORI <chirukkori@gmail.com>
2026-08-06 10:05:59 +0530

    Time: 51 ms (27.48%), Space: 84.9 MB (34.37%) - LeetHub

---
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+        int l=0, r=0, maxlen=0;
+        int n=fruits.size();
+        unordered_map<int,int>mpp;
+        while(r<n){
+            mpp[fruits[r]]++;
+            while(mpp.size()>2){
+                mpp[fruits[l]]--;
+                if(mpp[fruits[l]]==0){
+                    mpp.erase(fruits[l]);
+                }
+                l++;
+            }
+            maxlen=max(maxlen,r-l+1);
+            r++;
+        }
+         return maxlen;
+    }
+};
